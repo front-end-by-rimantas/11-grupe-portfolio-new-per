5bdc3441de1e5c91a913e8b90d6f60c72329671d
eimaaa <esalcius@gmail.com>
2019-11-22 11:48:12 +0200

    MY services #18

--- js/data.js
+++ js/data.js
@@ -13,7 +13,38 @@
 // counter section
 
 // My services
-
+const services = [
+    {
+        icon: 'ravelry',
+        title: 'BRANDING',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.'
+    },
+    {
+        icon: 'laptop',
+        title: 'WEB DESIGN',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.'
+    },
+    {
+        icon: 'shopping-cart',
+        title: 'MARKETING',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.'
+    },
+    {
+        icon: 'code',
+        title: 'CLEAN CODE',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.'
+    },
+    {
+        icon: 'tablet',
+        title: 'FRIENDELY ALL SCREAN',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.'
+    },
+    {
+        icon: 'phone',
+        title: 'UNLIMITED SUPPORT',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.'
+    }
+];
 // My Portfolio
 
 // testimonel
